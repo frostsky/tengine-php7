c4f7d8d943eb7e1918550824125c6adcee1b0fbc
frostsky <webv@live.cn>
2017-06-07 11:48:42 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-/usr/sbin/sshd -D
-
-/etc/init.d/php-fpm start
+#!/bin/bash
+/usr/sbin/sshd -D &
+/etc/init.d/php-fpm start > /dev/null
 /usr/local/nginx/sbin/nginx
+/usr/local/redis/bin/redis-server /usr/local/redis/redis.conf
